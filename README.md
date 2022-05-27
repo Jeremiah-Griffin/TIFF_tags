34f3be286c5205bad1c79f8abcd9e7439cb816cd
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2022-05-27 06:57:56 -0400

    Replace btreemap with a Btreeset and linear lookup to allow for seak by tag or ID number. Added a method to search by tag name.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# TIFF_tags
+# TIFF_tags_nightly
 
-A  look up table to return the name of a TIFF/EXIF tag given its u16 tag ID.
+Note: this version of TIFF_tags uses the Once_Cell feature from the nightly rust compiler 
+in lieu of the Lazy_Static! macro.
 
-MIT license. Please cite the United States Libarary of Congress: 
+
+A  lookup table to return the name of a TIFF/EXIF tag given its u16 tag ID.
+
+MIT license. Please cite the United States Library of Congress: 
 
 https://web.archive.org/web/20220412210014/https://www.loc.gov/preservation/digital/formats/content/tiff_tags.shtml
--- src/tests.rs
+++ src/tests.rs
@@ -1,28 +1,27 @@
 #[cfg(test)]
 
-mod tests{
-    use crate::get_name;
-    use crate::get_id;
+
+use crate::get_name;
+use crate::get_id;
 
 
-    #[test]
-    fn return_some_name(){
-      assert_eq!( get_name(254 as u16), Some(String::from("NewSubfileType")))
-    }
+#[test]
+fn return_some_name(){
+  assert_eq!( get_name(254 as u16), Some(String::from("NewSubfileType")))
+}
 
-    #[test]
-    fn return_none_name(){
-        assert_eq!(get_name(42069 as u16), None)
-    }
+#[test]
+fn return_none_name(){
+    assert_eq!(get_name(42069 as u16), None)
+}
 
-    #[test]
-    fn return_some_id(){
-        assert_eq!(get_id("NewSubfileType"), Some(254))
+#[test]
+fn return_some_id(){
+    assert_eq!(get_id("NewSubfileType"), Some(254))
 
-    }
+}
 
-    #[test]
-    fn return_none_id(){
-        assert_eq!(get_id("It Won't Be Long by Winston K. is a great song!"), None)
-    }
+#[test]
+fn return_none_id(){
+    assert_eq!(get_id("It Won't Be Long by Winston K. is a great song!"), None)
 }
